d32047cb2b7a524d73e45515cf183929fd6aeb04
bena1ad <beamoah@hillsidek12.net>
2025-03-26 08:47:15 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+import tkinter as tk
+
+
+window = tk.Tk()
+window.title("Memory Game")
+window.geometry("1000x1000")
+
+clicked = False
+
+def on_click():
+    clicked = True 
+    
+if clicked == True:
+    print("Button Clicked!")
+
+button = tk.Button(window, text=" START", command=on_click)
+button.pack()
+
+
+
+
+
+
+root.mainloop()
